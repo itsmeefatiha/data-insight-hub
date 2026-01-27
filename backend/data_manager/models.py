f785a9633aa3060b671eba5026cff28100eefddd
itsmeefatiha <fatihabouzar450@gmail.com>
2026-01-27 20:13:09 +0100

    remove the delimiter field from the DataSource model

--- backend/data_manager/models.py
+++ backend/data_manager/models.py
@@ -1,3 +1,4 @@
+import os
 from django.db import models
 from django.conf import settings
 from django.core.validators import FileExtensionValidator
@@ -6,6 +7,7 @@ class DataSource(models.Model):
     DATA_TYPES = (
         ('csv', 'CSV File'),
         ('excel', 'Excel File'),
+        ('xml', 'XML File'),
     )
 
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='data_sources')
@@ -13,21 +15,25 @@ class DataSource(models.Model):
     
     file = models.FileField(
         upload_to='uploads/%Y/%m/',
-        validators=[FileExtensionValidator(allowed_extensions=['csv', 'xlsx', 'xls'])]
+        validators=[FileExtensionValidator(allowed_extensions=['csv', 'xlsx', 'xls', 'xml'])]
     )
     
     file_type = models.CharField(max_length=10, choices=DATA_TYPES, default='csv')
     
-    delimiter = models.CharField(max_length=1, default=',')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def save(self, *args, **kwargs):
-        # Auto-detect type based on extension
-        if self.file.name.endswith('.csv'):
+        # 3. Enhanced Auto-detection
+        ext = os.path.splitext(self.file.name)[1].lower()
+        
+        if ext == '.csv':
             self.file_type = 'csv'
-        else:
+        elif ext in ['.xlsx', '.xls']:
             self.file_type = 'excel'
+        elif ext == '.xml':
+            self.file_type = 'xml'
+            
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f"{self.name} ({self.owner.email})"
+        return f"{self.name} ({self.file_type})"
