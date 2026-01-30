05e7f4296306ecc31f9aa39a76597290c2617419
itsmeefatiha <fatihabouzar450@gmail.com>
2026-01-30 21:24:26 +0100

    feat(analytics): add the endpoint for summary preview button

--- backend/analytics/views.py
+++ backend/analytics/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.pagination import PageNumberPagination
+from django.shortcuts import get_object_or_404
+from data_manager.models import DataSource
+from .models import AnalysisResult
+from .utils import load_dataset
+import pandas as pd
 
-# Create your views here.
+# --- 1. The Summary View (For the "Preview" Button) ---
+class AnalysisSummaryView(APIView):
+    def get(self, request, pk):
+        # Retrieve the pre-calculated summary from DB
+        data_source = get_object_or_404(DataSource, pk=pk, owner=request.user)
+        
+        try:
+            result = data_source.analysis # Access the OneToOne relation
+            return Response({
+                "filename": data_source.name,
+                "total_rows": result.total_rows,
+                "columns": result.column_types,
+                "summary": result.basic_stats
+            })
+        except AnalysisResult.DoesNotExist:
+            return Response({"status": "processing", "message": "Summary is being generated..."}, status=202)
